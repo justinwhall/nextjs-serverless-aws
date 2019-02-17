24dbd3604cf1dadf0a7004d1a6d9fd68a5123c3f
Justin W Hall <justinwhall@me.com>
2019-02-17 14:22:58 -0700

    fix nav

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Link from 'next/link'
 export default () => (
   <ul>
     <li>
-      <Link href='/b' as='/a'>
+      <Link href='/a' as='/a'>
         <a>a</a>
       </Link>
     </li>
     <li>
-      <Link href='/a' as='/b'>
+      <Link href='/b' as='/b'>
         <a>b</a>
       </Link>
     </li>
